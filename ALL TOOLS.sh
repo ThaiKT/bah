11b8ef00a607de7b3193e8e9b2b48a1cad905cbc
ThaiKT <72713252+ThaiKT@users.noreply.github.com>
2020-10-23 19:34:51 -0300

    Add files via upload

---
+++ ALL TOOLS.sh
@@ -0,0 +1,61 @@
+echo ""
+echo ""
+echo ""
+echo "Script por el Thai qn ma"
+echo ""
+echo ""
+echo ""
+echo "#XeniTeMeo"
+echo ""
+echo ""
+echo ""
+echo "se tan empezando a instalar vo calmao" 
+echo ""
+echo ""
+echo ""
+
+apt-get update -y                    
+apt install python -y           
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install toilet -y
+apt install figlet -y
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+pkg install openssh -y
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install fish -y
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+apt install tor -y
+apt install w3m -y
+apt install cmatrix -y
+apt install wcalc -y 
+apt install openssl -y
+apt install bmon -y
+apt-get upgrade -y
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo "Auto instalador de paquetes en termux"
+echo ""
+echo ""
+echo "ponganle kvros,, me di la paja de hacer esta wea xd"
+echo ""
+echo ""
+echo "Subscribe our Youtube channel and press Bell icon"
+exit
